fb6bec380254ce2ef6995cb0d3a8bb5653327cc1
Xavi <xaviregnier@gmail.com>
2020-06-01 23:23:41 +0200

    quest ok

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,12 @@
 package com.wildcodeschool.wildandwizard.repository;
 
 import com.wildcodeschool.wildandwizard.entity.School;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SchoolRepository {
 
@@ -10,7 +16,32 @@ public class SchoolRepository {
 
     public School save(String name, Long capacity, String country) {
 
-        // TODO : insert a new school in database
-        return null;
-    }
-}
+            try {
+                Connection connection = DriverManager.getConnection(
+                        DB_URL, DB_USER, DB_PASSWORD
+                );
+                PreparedStatement statement = connection.prepareStatement(
+                        "INSERT INTO school (name, capacity, country) VALUES (?, ?, ?)",
+                        Statement.RETURN_GENERATED_KEYS
+                );
+                statement.setString(1, name);
+                statement.setLong(2, capacity);
+                statement.setString(3, country);
+
+                if (statement.executeUpdate() != 1) {
+                    throw new SQLException("failed to insert data");
+                }
+
+                ResultSet generatedKeys = statement.getGeneratedKeys();
+
+                if (generatedKeys.next()) {
+                    Long id = generatedKeys.getLong(1);
+                    return new School(id, name, capacity, country);
+                } else {
+                    throw new SQLException("failed to get inserted id");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            };
+            return null;
+    }}
